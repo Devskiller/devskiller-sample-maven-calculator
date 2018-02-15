a41868181dcc735f3216e62b63411b3fc8b8dbb8
MuthuChess <35551343+MuthuChess@users.noreply.github.com>
2018-02-16 00:12:02 +0530

    Update Calculator.java
    Second Com

--- src/main/java/com/devskiller/calculator/Calculator.java
+++ src/main/java/com/devskiller/calculator/Calculator.java
@@ -3,18 +3,14 @@ package com.devskiller.calculator;
 public class Calculator {
 
     public int add(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     public int subtract(int a, int b) {
-        return 0;
+        return a - b;
     }
 
     public int multiply(int a, int b) {
-        return 0;
+        return a*b;
     }
-    public int divide(int a, int b) {
-        return 0;
-    }
-
 }
